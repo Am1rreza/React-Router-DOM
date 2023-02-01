381cb697662991627a446fb9d8e1099f9cc2e8dd
Amirreza <amirreza.m13825@gmail.com>
2023-02-01 20:41:41 +0330

    Route components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutUsPage from "./pages/AboutUsPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about-us" component={AboutUsPage} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -2,6 +2,7 @@ const AboutUsPage = (props) => {
   return (
     <>
       <h2>Wellcome to the about us page</h2>
+      <a href="/">Go to the Home page</a>
     </>
   );
 };
