22cd40775ca13d05a31221bb616ceb5bb413e368
Amirreza <amirreza.m13825@gmail.com>
2023-03-02 23:57:17 +0330

    replace activeClassName to className

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,33 +1,49 @@
 import { NavLink, useParams } from "react-router-dom";
 
-const Navigation = (props) => {
+const Navigation = () => {
   const params = useParams();
 
   return (
     <nav>
       <ul>
         <li>
-          <NavLink activeClassName="activeTab2" to="/" exact>
+          <NavLink
+            className={(navData) => (navData.isActive ? "activeTab2" : "")}
+            to="/"
+            exact
+          >
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink activeClassName="activeTab2" to="/about-us">
+          <NavLink
+            className={(navData) => (navData.isActive ? "activeTab2" : "")}
+            to="/about-us"
+          >
             About Us
           </NavLink>
         </li>
         <li>
-          <NavLink activeClassName="activeTab2" to="/profile">
+          <NavLink
+            className={(navData) => (navData.isActive ? "activeTab2" : "")}
+            to="/profile"
+          >
             Profile
           </NavLink>
         </li>
         <li>
-          <NavLink activeClassName="activeTab2" to="/blogs">
+          <NavLink
+            className={(navData) => (navData.isActive ? "activeTab2" : "")}
+            to="/blogs"
+          >
             Blog
           </NavLink>
         </li>
         <li>
-          <NavLink activeClassName="activeTab2" to="/post">
+          <NavLink
+            className={(navData) => (navData.isActive ? "activeTab2" : "")}
+            to="/post"
+          >
             Post
           </NavLink>
         </li>
